bac496a99279e8c19f02afd2b7a76c262a3002b0
kasiakulesz <kulesz.katarzyn@gmail.com>
2025-06-13 21:13:27 +0200

    working delete button added

--- src/main.js
+++ src/main.js
@@ -48,6 +48,12 @@ async function fetchArticles() {
 }).join('');
 
     document.getElementById("articles").innerHTML = articleList;
+
+    const { data: session } = await supabase.auth.getSession();
+    if (session) {
+      setupDeleteButton();
+      setupEditButton();
+    }
 }
 
 function setupLoginButton() {
@@ -65,7 +71,7 @@ function setupLogoutButton() {
   const navbar = document.querySelector('nav');
 
   const logoutButton = document.createElement('a');
-  logoutButton.textContent = 'Logout';
+  logoutButton.textContent = 'Wyloguj się';
   logoutButton.type = "button";
   logoutButton.className = 'text-l bg-pink-300 text-white font-semibold px-3 py-1 rounded-full transition duration-500 ease-in-out hover:bg-blue-300 cursor-pointer';
 
@@ -96,8 +102,8 @@ function setupAddArticleButton () {
     const dialog = document.createElement('dialog');
     dialog.className = 'bg-pink-100';
     dialog.innerHTML = `
-      <section class="m-auto w-fit bg-pink-100 text-pink-800 align-text-bottom rounded mb-50">
-        <form id="add-article-form" class="p-5 inline-grid gap-2 align-baseline">
+      <section class="fixed inset-0 z-10 overflow-y-auto flex justify-center items-center text-pink-800 align-text-bottom rounded">
+        <form id="add-article-form" class="bg-pink-100 p-5 inline-grid gap-2 align-baseline">
           <h2 class="text-2xl font-bold col-span-4">Dodaj nowy artykuł</h2>
           <label for="title" class="mt-2 col-span-4">Tytuł: </label>
             <input type="text" id="title" class="bg-white col-span-4 rounded p-1 focus:outline-pink-800" required/>
@@ -109,29 +115,84 @@ function setupAddArticleButton () {
             <input type="datetime-local" id="date" name="created_at" class="mt-2 bg-white col-span-1 p-1 rounded focus:outline-pink-800" required />
           <label for="content" class="mt-2">Treść: </label>
             <textarea id="content" class="bg-white col-span-4 rounded p-1 focus:outline-pink-800" required></textarea>
-          <button type="submit" id="submit" class="cursor-pointer bg-pink-300 p-2 mt-2 w-auto justify-self-center rounded text-white font-semibold col-span-4 transition duration-500 ease-in-out">Dodaj na stronę</button>
+          <button type="submit" id="submit" class="cursor-pointer bg-pink-300 p-2 mt-2 w-auto justify-self-center rounded text-white font-semibold col-span-4 hover:bg-blue-300 transition duration-500 ease-in-out">Dodaj na stronę</button>
+          <button type="button" id="close" aria-label="close" class="cursor-pointer hover:bg-pink-400 p-1 mt-1 w-auto justify-self-center rounded text-white font-semibold text-xs col-span-4 bg-blue-300 transition duration-500 ease-in-out" formnovalidate>Anuluj</button>
         </form>
       </section>
       `;
     
     document.body.appendChild(dialog);
-    dialog.showModal()
+    dialog.showModal();
+
+    const closeButton = dialog.querySelector('#close');
+    closeButton.addEventListener('click', (e) => {
+      e.preventDefault();
+      dialog.close();
+      dialog.remove();
+    });
 
     const form = dialog.querySelector('#add-article-form');
     form.addEventListener('submit', async (e) => {
       e.preventDefault();
-      const title = e.target.title.value;
-      const content = e.target.content.value;
+        const articleData = {
+          title: form.title.value,
+          subtitle: form.subtitle.value,
+          author: form.author.value,
+          created_at: form.created_at.value ? new Date(form.created_at.value).toISOString() : new Date().toISOString(),
+          content: form.content.value
+        };
 
-      const { error } = await supabase.from('articles').insert({ title, content });
+      const { error } = await supabase.from('articles').insert(articleData);
 
       if (error) {
         console.error('Error adding article:', error);
         alert('Błąd podczas dodawania artykułów. Sprawdź konsolę.');
         return;
       }
+
+      dialog.close();
+      await fetchArticles();
     });
   });
 
   navbar.appendChild(addArticleButton);
+}
+
+function setupDeleteButton() {
+  document.querySelectorAll(".article").forEach(single_article => {
+    const articleID = single_article.dataset.articleId;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = "Usuń";
+    deleteButton.type = "button";
+    deleteButton.className = "cursor-pointer hover:bg-pink-400 p-1 mt-1 w-auto justify-self-center rounded text-white font-semibold text-xs col-span-4 bg-blue-300 transition duration-500 ease-in-out mr-1";
+
+    deleteButton.addEventListener('click', async () => {
+      const { error } = await supabase
+        .from('articles')
+        .delete()
+        .eq('id', articleID)
+
+      if (error) {
+        console.error('Wystąpił błąd podczas usuwania: ', erorr);
+        alert('Usunięcie nie powiodło się.')
+        return;
+      }
+
+    await fetchArticles();
+    });
+
+    single_article.appendChild(deleteButton);
+  });
+}
+
+function setupEditButton() {
+  document.querySelectorAll(".article").forEach(single_article => {
+    const editButton = document.createElement('button');
+    editButton.textContent = "Edytuj";
+    editButton.type = "button";
+    editButton.className = "cursor-pointer hover:bg-pink-400 p-1 mt-1 w-auto justify-self-center rounded text-white font-semibold text-xs col-span-4 bg-blue-300 transition duration-500 ease-in-out";
+
+    single_article.appendChild(editButton);
+  });
 }

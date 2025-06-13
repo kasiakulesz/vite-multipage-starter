534841a2d6726c48be53a97575fb4253bd4d09b7
kasiakulesz <kulesz.katarzyn@gmail.com>
2025-06-13 18:00:24 +0200

    login button added

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ main();
 
 async function main() {
   
+  setupLoginButton();
+  
   await fetchArticles();
 }
 
@@ -21,18 +23,29 @@ async function fetchArticles() {
   console.log('Fetched articles:', data);
   const articleList = data.map((article, i) => {
     return `
-      ${i > 0 ? '<hr />' : ''}
+      ${i > 0 ? '<hr class="w-50 h-px mx-auto my-8 bg-gray-300 border-0"/>' : ''}
       <article class="article" data-article-id="${article.id}">
         <h2 class="font-bold text-xl">${article.title}</h2>
+        <h3 class="text-l font-semibold">${article.subtitle}</h4>
         <div class="flex gap-2 text-sm">
           <address rel="author">${article.author}</address>
           <time datetime="${article.created_at}">${new Date(article.created_at).toLocaleDateString()}</time>
         </div>
-
         <p class="mt-2">${article.content}</p>
       </article>
     `;
 }).join('');
 
     document.getElementById("articles").innerHTML = articleList;
+}
+
+function setupLoginButton() {
+  const navbar = document.querySelector('nav');
+
+  const loginButton = document.createElement('a');
+  loginButton.textContent = 'Login';
+  loginButton.href = "/login/";
+  loginButton.className = 'text-l bg-pink-300 text-white font-semibold px-3 py-1 rounded-full transition duration-500 ease-in-out hover:bg-blue-300';
+
+  navbar.appendChild(loginButton);
 }
